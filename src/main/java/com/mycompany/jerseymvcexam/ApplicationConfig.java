d5ebc0d60e1bb78c46f6b9895f47760571c9c59f
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2015-07-09 01:51:37 +0900

    Change ApplicationConfig to ResourceConfig

--- src/main/java/com/mycompany/jerseymvcexam/ApplicationConfig.java
+++ src/main/java/com/mycompany/jerseymvcexam/ApplicationConfig.java
@@ -1,35 +1,15 @@
 package com.mycompany.jerseymvcexam;
 
-import java.util.Set;
-import javax.ws.rs.core.Application;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.mvc.mustache.MustacheMvcFeature;
 
-/**
- *
- * @author hondou
- */
 @javax.ws.rs.ApplicationPath("webresources")
-public class ApplicationConfig extends Application {
-        
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<>();
-        // Jersey MVC
-        resources.add(MustacheMvcFeature.class);
-        
-        // Rest Resource Classes
-        addRestResourceClasses(resources);
-        return resources;
-    }
-
-    /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
-     */
-    private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(com.mycompany.jerseymvcexam.GenericResource.class);
-    }
+public class ApplicationConfig extends ResourceConfig {
     
+    public ApplicationConfig() {
+        this
+        .packages(ApplicationConfig.class.getPackage().getName())
+        // .property(MustacheMvcFeature.TEMPLATE_BASE_PATH, "/Users/atsushi/templates")
+        .register(MustacheMvcFeature.class);
+    }    
 }
